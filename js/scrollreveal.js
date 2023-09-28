74b4bac0372174e90ed781445ca0254e864ef941
Cano's Code  (；●∀●)ゝ” <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2023-09-28 17:57:35 -0600

    FirstCommit

---
+++ js/scrollreveal.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.centrado', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-150px'
+    });
+
+    sr.reveal('.cards',{
+        duration: 2000,
+        origin:'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.scroll',{
+        duration:2000,
+        origin:'bottom',
+        distance:'-150px'
+    })
